024b9be226d12e4166a45670bb253eb483179c96
Canvasbag989 <Canvasbag989@icloud.com>
2019-09-09 17:20:08 +0100

    installed pg and connected to database

--- lib/bookmark.rb
+++ lib/bookmark.rb
@@ -1,7 +1,11 @@
+require 'pg'
 class Bookmark
   def self.all
-    [
-      "https://github.com/"
-    ]
+    connection = PG.connect(dbname: 'bookmark_manager')
+    result = connection.exec('SELECT * FROM bookmarks;')
+    result.map { |bookmark| bookmark["url"] }
+    # [
+    #   "https://github.com/"
+    # ]
   end
 end
